d0de2730902e1bca252be7a7dd942cfa0d13f89a
Tyrfing Mjolner <4lphamal3@gmail.com>
2014-02-17 08:53:07 +0700

    Create FMError.h

---
+++ FMError.h
@@ -0,0 +1,33 @@
+#pragma mark -
+
+#import <Foundation/Foundation.h>
+
+@interface FMError : NSObject
+
+@property( nonatomic, strong ) NSDictionary *errorList;
+
+@end
+
+#pragma mark - Copyright stuff
+
+/*
+ 
+ FX.m
+ cocoaFX
+ 
+ Created by Gjermund Gusland Thorsen on 13.02.14.
+ Copyright (c) 2014 Gjermund Gusland Thorsen. All rights reserved.
+ 
+ 
+ Based on the mothership of all FileMaker CWP solutions: FX.php 6.0
+ by Chris Hansen with Chris Adams, G G Thorsen, Masayuki Nii, and others
+ 
+ Like it's mother project; FX.php, cocoaFX is also based on the same
+ license namely: Artistic License and addendum (included with release)
+ 
+ cocoaFX is a free open-source PHP class for accessing FileMaker and other databases.
+ 
+ http://github.com/tyrfingmjolnir/cocoafx/
+ */
+
+#pragma mark - In the memory of my tribe; May Pahana be with you
